becac1e2b7fae0f5d7b8968f38efb27d04061f20
1n5aN1aC <joshuavillwo@gmail.com>
2016-04-29 12:26:08 -0700

    change to more efficient weather query

--- framework/weather.py
+++ framework/weather.py
@@ -7,11 +7,11 @@ import settings
 f = "error"
 
 def fetchWeather(forecast_type):
-	global f
-	url = 'http://api.wunderground.com/api/' + settings.key + '/geolookup/' + forecast_type +  '/q/' + settings.zipcode + '.json'
-	f = urllib2.urlopen(url)
-	json_string = f.read()
-	return json.loads(json_string)
+    global f
+    url = 'http://api.wunderground.com/api/' + settings.key + "/" + forecast_type +  '/q/' + settings.zipcode + '.json'
+    f = urllib2.urlopen(url)
+    json_string = f.read()
+    return json.loads(json_string)
 
 def closeURL():
-	f.close()
+    f.close()
